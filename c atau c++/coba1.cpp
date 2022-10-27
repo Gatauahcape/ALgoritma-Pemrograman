ecfa2e091e556b5074a30dfc2a1adb56b80f52ad
Gatauahcape <anggaajah679@gmail.com>
2022-10-27 11:08:16 +0700

    Menambahkan latihan
    Menambahkan latihan algoritma dan pemrograman

---
+++ c atau c++/coba1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+    int a = 7;
+    int b = 2;
+    char c[25]="Muhammad Angga Nugraha";
+
+    puts("Nama");
+    puts(c);
+    print("Terdiri dari %d hurup dan %d kata".a,b);
+}
---
+++ python/tipedata.py
@@ -0,0 +1,36 @@
+print('---Belajar Tipe Data---')
+
+# Tipe data integers
+y = 3
+z = 5 + 3
+print(z)
+# Tipe data floating numbers
+a = 7.5
+print(a)
+
+# Menggunakan single quote
+myFirstName = 'Muhammad'
+
+# Menggunakan double quote
+myLastName = 'Angga Nugraha'
+
+# Penulisan Angka sebagai string
+myAge = '18'
+
+# penulisan string mulltiline
+myHobby = """Hobi saya adalah Bermain game."""
+
+# mencetak 2 variabel string depan dengan perintah print
+print(myFirstName + " " + myLastName)
+print("Umur saya : " + myAge)
+print("Hobby saya : " + myHobby)
+
+#contoh penulisan boolean yang benar
+a = True
+b = False
+
+#contoh penulisan boolean yang salah
+#jika dijalankan, pada baris ini akan error
+c = a
+
+print(c)
